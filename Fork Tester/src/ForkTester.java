cdd14bf34a0b8d93cc9edeba00ef87537cc17afc
ryan.manning <ryan.manning@MU-C3-02.mullen.pvt.k12.co.us>
2014-08-26 08:22:24 -0600

    Initial Commit at School

--- Fork Tester/src/ForkTester.java
+++ Fork Tester/src/ForkTester.java
@@ -6,6 +6,10 @@ public class ForkTester
 		// Try what you did before.  
 		// Fork this to your personal GitHub account.
 		// Download it to school and add a line.
+		
+		//Downloaded this at school and added this line today 8/26
+		
+		
 		// Commit that back to your GitHub account.
 		// Download it to your home local repo.
 		// Commit that back to GitHub.
